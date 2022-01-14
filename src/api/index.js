1f9ffc4626bdebe51347bd25b57956dab2af83da
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-13 18:37:03 -0800

    API Setup
    - API Setup

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+let baseURL
+let API
+if (process.env.REACT_APP_NODE_ENV === 'development') {
+  baseURL = process.env.REACT_APP_SERVER_DEVELOPMENT_URL
+  API = axios.create({ baseURL })
+  API.interceptors.request.use((req) => {
+    if (localStorage.getItem('user')) {
+      req.headers.Authorization = `Bearer ${
+        JSON.parse(localStorage.getItem('profile')).token
+      }`
+    }
+    return req
+  })
+}
+
+if (process.env.REACT_APP_NODE_ENV === 'production') {
+  baseURL = '/api/v1'
+  API = axios.create({ baseURL })
+}
+
+// Auth
+export const register = (formData) => API.post('/auth/register', formData)
+export const login = (formData) => API.post('/auth/login', formData)
+export const logout = () => API.post('/auth/logout')
+
+// Posts
+export const getPosts = () => API.get('/posts')
