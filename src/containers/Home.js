139fd64e6919ba7111675329aad8b1bfd517b4e7
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-14 02:32:07 -0800

    Home Page
     - Home Page
     - Home Container

---
+++ src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HomeContainer = ({ children }) => {
+  return (
+    <div className='bg-gray-100 w-full min-h-screen p-10 border-orange-600'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 border-orange-600'>
+        <>{children}</>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { HomeContainer } from '../containers/Home'
+import { Post } from '../components'
+import { getPosts } from '../store/actions/posts'
 
 export const Home = () => {
-  return <>Home Page</>
+  const dispatch = useDispatch()
+  const posts = useSelector((state) => state.posts)
+
+    useEffect(() => {
+      dispatch(getPosts())
+    }, [dispatch])
+  
+  return (
+    <HomeContainer>
+      {posts.map(post => (
+        <Post key={post._id}/>
+    ))}
+    </HomeContainer>
+  )
 }
